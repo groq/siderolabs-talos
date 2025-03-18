josephglanville/talos

feat: Run Talos entirely out of RAM

internal/app/machined/pkg/runtime/v1alpha1/v1alpha1_sequencer.go
@@ -185,13 +185,25 @@ func (*Sequencer) Install(r runtime.Runtime) []runtime.Phase {
 			).Append(
 				"unmountState",
 				UnmountStatePartition,
-			).Append(
+			).AppendWithDeferredCheck(
+				func() bool {
+					wipe := procfs.ProcCmdline().Get(constants.KernelParamRamdisk).First()
+					return pointer.SafeDeref(wipe) == ""
+				},
 				"stopEverything",
 				StopAllServices,
-			).Append(
+			).AppendWithDeferredCheck(
+				func() bool {
+					wipe := procfs.ProcCmdline().Get(constants.KernelParamRamdisk).First()
+					return pointer.SafeDeref(wipe) == ""
+				},
 				"kexec",
 				KexecPrepare,
-			).Append(
+			).AppendWithDeferredCheck(
+				func() bool {
+					wipe := procfs.ProcCmdline().Get(constants.KernelParamRamdisk).First()
+					return pointer.SafeDeref(wipe) == ""
+				},
 				"reboot",
 				Reboot,
 			)

groq/siderolabs-talos

internal/app/machined/pkg/runtime/v1alpha1/v1alpha1_sequencer.go
@@ -181,16 +181,32 @@ func (*Sequencer) Install(r runtime.Runtime) []runtime.Phase {
 			).Append(
 				"unmountState",
 				UnmountStatePartition,
-			).Append(
+			).AppendWithDeferredCheck(
+				func() bool {
+					ramdisk := procfs.ProcCmdline().Get(constants.KernelParamRamdisk).First()
+					return pointer.SafeDeref(ramdisk) == ""
+				},
 				"volumeFinalize",
 				TeardownVolumeLifecycle,
-			).Append(
+			).AppendWithDeferredCheck(
+				func() bool {
+					ramdisk := procfs.ProcCmdline().Get(constants.KernelParamRamdisk).First()
+					return pointer.SafeDeref(ramdisk) == ""
+				},
 				"stopEverything",
 				StopAllServices,
-			).Append(
+			).AppendWithDeferredCheck(
+				func() bool {
+					ramdisk := procfs.ProcCmdline().Get(constants.KernelParamRamdisk).First()
+					return pointer.SafeDeref(ramdisk) == ""
+				},
 				"kexec",
 				KexecPrepare,
-			).Append(
+			).AppendWithDeferredCheck(
+				func() bool {
+					ramdisk := procfs.ProcCmdline().Get(constants.KernelParamRamdisk).First()
+					return pointer.SafeDeref(ramdisk) == ""
+				},
 				"reboot",
 				Reboot,
 			)